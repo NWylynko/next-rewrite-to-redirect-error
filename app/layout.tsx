248d0ec4211638542b7e90061f63424924b6f037
Nick Wylynko <nickwylynko@clerk.dev>
2025-12-12 02:22:08 +0800

    add clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default function RootLayout({
@@ -6,13 +7,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>
-        <nav>
-          <Link href="/">Home</Link>
-        </nav>
-        <main>{children}</main>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <nav>
+            <Link href="/">Home</Link>
+          </nav>
+          <main>{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,29 @@
-import { NextResponse } from "next/server";
+import { NextFetchEvent, NextResponse } from "next/server";
 import { NextRequest } from "next/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export const middleware = (request: NextRequest) => {
-  if (request.nextUrl.pathname.startsWith("/docs")) {
-    const [, , ...slug] = request.nextUrl.pathname.split("/");
+export const middleware = (request: NextRequest, event: NextFetchEvent) => {
+  return clerkMiddleware(async (auth, request, event) => {
+    if (
+      (request.method === "GET" || request.method === "HEAD") &&
+      request.nextUrl.pathname.startsWith("/docs")
+    ) {
+      const [, , ...slug] = request.nextUrl.pathname.split("/");
 
-    const dynamic = Math.random() > 0.5 ? "cats" : "dogs";
+      const dynamic = Math.random() > 0.5 ? "cats" : "dogs";
 
-    return NextResponse.rewrite(
-      new URL(`/docs/${dynamic}/${slug.join("/")}`, request.url)
-    );
-  }
+      return NextResponse.rewrite(
+        new URL(`/docs/${dynamic}/${slug.join("/")}`, request.url)
+      );
+    }
+  })(request, event);
 };
 
 export const config = {
-  matcher: "/:path*",
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
