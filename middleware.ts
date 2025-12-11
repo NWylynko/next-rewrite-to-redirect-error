cb98d3092f7f583c936e514046ca4e0e25b8d35f
Nick Wylynko <nickwylynko@clerk.dev>
2025-12-12 01:23:58 +0800

    downgrade to next 15

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,12 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
-export const proxy = (request: NextRequest) => {
+export const middleware = (request: NextRequest) => {
   if (request.nextUrl.pathname === "/page-1") {
     return NextResponse.rewrite(new URL("/page-2", request.url));
   }
+};
+
+export const config = {
+  matcher: "/:path*",
 };
